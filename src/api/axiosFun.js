7324701837686521ddc90d27fd59bbe285098609
kkk <123456@163.com>
2021-05-24 18:16:08 +0800

    添加请求、响应拦截初步实现

--- src/api/axiosFun.js
+++ src/api/axiosFun.js
@@ -56,3 +56,38 @@ export {
     loginreq,
     req
 }
+
+
+import {Message,Loading} from 'element-ui'
+import router from '../router'
+
+// let loading ;
+//  请求拦截
+axios.interceptors.request.use(config=> {
+  console.info(11)
+  Loading.service({text:"Loading..."});
+  return config;
+}, err=> {
+  Message.error({message: '请求超时!'});
+  return Promise.resolve(err);
+})
+//  响应拦截
+axios.interceptors.response.use(res=> {
+  console.info(res.status)
+  console.info(222)
+  Loading.service().close();
+  return res;
+}, function (err) {
+  Loading.service().close();
+  console.info(err.response)
+  console.info(333)
+  if (err.response.status == 504||err.response.status == 404) {
+    Message.error({message: '服务器被吃了⊙﹏⊙∥'});
+  } else if (err.response.status == 403) {
+    Message.error({message: '权限不足,请联系管理员!'});
+  } else {
+    Message.error({message: '未知错误'});
+  }
+  return Promise.reject(err);
+})
+
